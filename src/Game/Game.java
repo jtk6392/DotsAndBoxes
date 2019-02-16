4255bce833feaadc6c4709c999b47541ba8f707e
Kyle Zielinski <kizielinski219@gmail.com>
2019-02-16 16:01:54 -0500

    Game Class Changes-Overrides Textures

--- src/Game/Game.java
+++ src/Game/Game.java
@@ -1,7 +1,30 @@
 package Game;
 
-public class Game {
+import java.awt.*;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.Rectangle2D;
+import java.awt.image.ColorModel;
+
+public class Game implements Paint {
+
+    Board dots;
+    //Loads in all data nec essary in the game
+    public void Intialize()
+    {
+        dots = new Board();
+        dots = new Board(10, 10);
+    }
     public static void main(String[] args) {
         System.out.println("Hello, world");
+    }
+
+    @Override
+    public PaintContext createContext(ColorModel cm, Rectangle deviceBounds, Rectangle2D userBounds, AffineTransform xform, RenderingHints hints) {
+        return null;
+    }
+
+    @Override
+    public int getTransparency() {
+        return 0;
     }
 }
