a892affe96e58b54254ef3925c78b791af5c289c
nsp5488 <nsp5488@g.rit.edu>
2019-02-16 14:56:26 -0500

    Continued AI

--- src/HardCodeAI/AI.java
+++ src/HardCodeAI/AI.java
@@ -1,22 +1,44 @@
 package HardCodeAI;
 
 import Game.Board;
-
+import Game.Users;
+import java.util.Random;
 public class AI {
     private Board currentBoard;
-    public AI(Board currentBoard) {
+    private Users player;
+
+    public AI(Board currentBoard, Users player) {
         this.currentBoard = currentBoard;
+        this.player = player;
     }
-    public boolean determinePointMove() {
+    private boolean determinePointMove() {
         return false;
     }
 
-    public boolean determineSafeMove() {
+    private boolean determineSafeMove() {
         return false;
     }
 
     public void makePlay(Board currentBoard) {
         this.currentBoard = currentBoard;
+        boolean madeMove = false;
+        int i, j;
 
+        if(!determinePointMove() && determineSafeMove()) {
+
+            madeMove = true;
+        }
+        if(determinePointMove() && determineSafeMove() && !madeMove) {
+            // take Point move
+            madeMove = true;
+        }
+        if(determinePointMove() && !determineSafeMove() && !madeMove) {
+            // recurse
+            madeMove = true;
+        }
+        if(!determinePointMove() && !determineSafeMove() && !madeMove) {
+            //
+            madeMove = true;
+        }
     }
 }
