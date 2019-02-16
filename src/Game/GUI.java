312a7b420a6f89c610461bcb655203d38d75b21d
Kyle Zielinski <kizielinski219@gmail.com>
2019-02-16 17:50:59 -0500

    Added basic window function

--- src/Game/GUI.java
+++ src/Game/GUI.java
@@ -1,16 +1,34 @@
 package Game;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.event.EventHandler;
 
 public class GUI extends Application {
+
+    Button button;
 
     public static void main(String[] args) {
         launch(args);
     }
 
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Title of the Window");
+
+        button = new Button();
+        button.setText("Click Me");
+
+        StackPane layout = new StackPane();
+        layout.getChildren().add(button);
+
+        Scene scene = new Scene(layout, 300, 250);
+        primaryStage.setScene(scene);
+        primaryStage.show();
 
     }
 
