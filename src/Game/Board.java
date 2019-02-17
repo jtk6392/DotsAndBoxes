f7bd065f5e42e4d289745a8a277a53e0a6f927f7
Kyle Zielinski <kizielinski219@gmail.com>
2019-02-17 03:10:49 -0500

    GUI Version 1

--- src/Game/Board.java
+++ src/Game/Board.java
@@ -1,112 +1,115 @@
-package Game;
-
-public class Board {
-
-    /**
-     * Creates the box array for the i and j index.
-     */
-    private Box[][] boxArray;
-
-    /**
-     * Creates score declaration.
-     */
-    private int[] score;
-
-    /**
-     * Creates a standard size for the board if the user does not select
-     * a size.
-     * Board Size: 4 x 4
-     */
-    public Board() {
-        this(4, 4);
-    }
-
-    /**
-     * score[0]: total player 1
-     * score[1]: total player 2
-     * score[2]: total score
-     *
-     * @param i index of the row
-     * @param j index of the column
-     */
-    public Board(int i, int j) {
-        boxArray = new Box[i][j];
-        for (int k = 0; k < i; k++) {
-            for (int l = 0; l < j; l++) {
-                boxArray[k][l] = new Box(k, l);
-            }
-        }
-        score = new int[3];
-        score[2] = i * j;  // total score
-    }
-
-    public int getBoardSize() {
-        return this.boxArray.length;
-    }
-
-    /**
-     * Sets the state of the size of the box to true
-     * and update and check and see if a box if finished
-     *
-     * @param bs     box side
-     * @param player the player
-     * @param i      index of the row
-     * @param j      index of the column
-     */
-    public static void Play(Box.Side bs, Users player, int i, int j) {
-
-    }
-
-
-    public static void claimSharedSide(int i, int j, Box.Side s) {
-
-    }
-
-    @Override
-    public String toString() {
-        return "No";
-    }
-
-    public static void Win() {
-
-    }
-
-    public boolean hasPartner(int i, int j, Game.Box.Side s) {
-        return hasPartner(getBox(i, j), s);
-    }
-
-    public boolean hasPartner(Box b, Game.Box.Side s) {
-        switch (s) {
-            case EAST:
-                return b.getxVal() == getBoardSize();
-            case WEST:
-                return b.getxVal() == 0;
-            case NORTH:
-                return b.getyVal() == 0;
-            case SOUTH:
-                return b.getyVal() == getBoardSize();
-        }
-    }
-
-    public Box getPartner(int i, int j, Game.Box.Side s) {
-        return getPartner(getBox(i, j), s);
-    }
-
-    public Box getPartner(Box b, Game.Box.Side s) {
-        switch (s) {
-            case EAST:
-                return this.boxArray[b.getxVal() + 1][b.getyVal()];
-            case WEST:
-                return this.boxArray[b.getxVal() - 1][b.getyVal()];
-            case NORTH:
-                return this.boxArray[b.getxVal()][b.getyVal() + 1];
-            case SOUTH:
-                return this.boxArray[b.getxVal()][b.getyVal() - 1];
-        }
-    }
-
-    public Box getBox(int i, int j) {
-        return boxArray[i][j];
-    }
-
-}
+//package Game;
+//
+//public class Board {
+//
+//    /**
+//     * Creates the box array for the i and j index.
+//     */
+//    private Box[][] boxArray;
+//
+//    /**
+//     * Creates score declaration.
+//     */
+//    private int[] score;
+//
+//    /**
+//     * Creates a standard size for the board if the user does not select
+//     * a size.
+//     * Board Size: 4 x 4
+//     */
+//    public Board() {
+//        this(4, 4);
+//    }
+//
+//    /**
+//     * score[0]: total player 1
+//     * score[1]: total player 2
+//     * score[2]: total score
+//     *
+//     * @param i index of the row
+//     * @param j index of the column
+//     */
+//    public Board(int i, int j) {
+//        boxArray = new Box[i][j];
+//        for (int k = 0; k < i; k++) {
+//            for (int l = 0; l < j; l++) {
+//                boxArray[k][l] = new Box(k, l);
+//            }
+//        }
+//        score = new int[3];
+//        score[2] = i * j;  // total score
+//    }
+//
+//    public int getBoardSize() {
+//        return this.boxArray.length;
+//    }
+//
+//    /**
+//     * Sets the state of the size of the box to true
+//     * and update and check and see if a box if finished
+//     *
+//     * @param bs     box side
+//     * @param player the player
+//     * @param i      index of the row
+//     * @param j      index of the column
+//     */
+//    public static void Play(Box.Side bs, Users player, int i, int j) {
+//
+//    }
+//
+//
+//    public static void claimSharedSide(int i, int j, Box.Side s) {
+//
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "No";
+//    }
+//
+//    public static void Win() {
+//
+//    }
+//
+//    public boolean hasPartner(int i, int j, Game.Box.Side s) {
+//        return hasPartner(getBox(i, j), s);
+//    }
+//
+//    public boolean hasPartner(Box b, Game.Box.Side s) {
+//        switch (s) {
+//            case EAST:
+//                return b.getxVal() == getBoardSize();
+//            case WEST:
+//                return b.getxVal() == 0;
+//            case NORTH:
+//                return b.getyVal() == 0;
+//            case SOUTH:
+//                return b.getyVal() == getBoardSize();
+//        }
+//        //Check this
+//        return false;
+//    }
+//
+//    public Box getPartner(int i, int j, Game.Box.Side s) {
+//        return getPartner(getBox(i, j), s);
+//    }
+//
+//    public Box getPartner(Box b, Game.Box.Side s) {
+//        switch (s) {
+//            case EAST:
+//                return this.boxArray[b.getxVal() + 1][b.getyVal()];
+//            case WEST:
+//                return this.boxArray[b.getxVal() - 1][b.getyVal()];
+//            case NORTH:
+//                return this.boxArray[b.getxVal()][b.getyVal() + 1];
+//            case SOUTH:
+//                return this.boxArray[b.getxVal()][b.getyVal() - 1];
+//        }
+//        return null;
+//    }
+//
+//    public Box getBox(int i, int j) {
+//        return boxArray[i][j];
+//    }
+//
+//}
--- src/HardCodeAI/AI.java
+++ src/HardCodeAI/AI.java
@@ -1,125 +1,125 @@
-package HardCodeAI;
-
-import Game.Board;
-import Game.Box;
-import Game.Users;
-
-import java.util.Random;
-
-public class AI {
-    private Board currentBoard;
-    private Users player;
-    private final static Random rng = new Random();
-
-    public AI(Board currentBoard, Users player) {
-        this.currentBoard = currentBoard;
-        this.player = player;
-    }
-
-    private int claimedEdges(int i, int j, Box.Side side) {
-        Box box1 = currentBoard.getBox(i, j);
-        return claimedEdges(box1);
-    }
-
-    private int claimedEdges(Box b) {
-        int filledSides = 0;
-        if (b.getSide(Box.Side.NORTH)) {
-            filledSides++;
-        }
-        if (b.getSide(Box.Side.SOUTH)) {
-            filledSides++;
-        }
-        if (b.getSide(Box.Side.EAST)) {
-            filledSides++;
-        }
-        if (b.getSide(Box.Side.WEST)) {
-            filledSides++;
-        }
-        return filledSides;
-    }
-
-    private boolean checkMove(int i, int j, Box.Side side) {
-        if (currentBoard.hasPartner(currentBoard.getBox(i, j), side)) {
-            Box newBox = currentBoard.getPartner(i, j, side);
-            return claimedEdges(i, j, side)<=2 && claimedEdges(newBox)<=2;
-        } else {
-            return claimedEdges(i, j, side)<=2;
-        }
-
-    }
-
-    private boolean scorableBox(Box b){
-        return claimedEdges(b)==3;
-    }
-
-    private boolean scorableBox(int i, int j){
-        return scorableBox(currentBoard.getBox(i, j));
-    }
-
-    private boolean determinePointMove() {
-        for (int i = 0; i < currentBoard.getBoardSize(); i++) {
-            for (int j = 0; j < currentBoard.getBoardSize(); j++) {
-                Box currentBox =
-            }
-        }
-
-        return false;
-    }
-
-    private boolean determineSafeMove() {
-        return false;
-    }
-
-    private void randomMove() {
-        int i, j, max, s;
-        Box.Side side = null;
-        max = 0;
-
-        i = rng.nextInt(max) + 1;
-        j = rng.nextInt(max) + 1;
-        s = rng.nextInt(4);
-        switch (s) {
-            case 0:
-                side = Box.Side.EAST;
-                break;
-            case 1:
-                side = Box.Side.SOUTH;
-                break;
-            case 2:
-                side = Box.Side.WEST;
-                break;
-            case 3:
-                side = Box.Side.NORTH;
-        }
-        if (checkMove(i, j, side)) {
-            Board.Play(side, this.player, i, j);
-        }
-    }
-
-    public void makePlay(Board currentBoard) {
-        this.currentBoard = currentBoard;
-        boolean madeMove = false;
-        while (!madeMove) {
-
-            if (!determinePointMove() && determineSafeMove()) {
-                randomMove();
-                madeMove = true;
-            }
-
-            if (determinePointMove() && determineSafeMove() && !madeMove) {
-                // take Point move
-                madeMove = true;
-            }
-
-            if (determinePointMove() && !determineSafeMove() && !madeMove) {
-                // recurse
-                madeMove = true;
-            }
-
-            if (!determinePointMove() && !determineSafeMove() && !madeMove) {
-                //
-                madeMove = true;
-            }
-        }
-    }
-}
+//package HardCodeAI;
+//
+//import Game.Board;
+//import Game.Box;
+//import Game.Users;
+//
+//import java.util.Random;
+//
+//public class AI {
+//    private Board currentBoard;
+//    private Users player;
+//    private final static Random rng = new Random();
+//
+//    public AI(Board currentBoard, Users player) {
+//        this.currentBoard = currentBoard;
+//        this.player = player;
+//    }
+//
+//    private int claimedEdges(int i, int j, Box.Side side) {
+//        Box box1 = currentBoard.getBox(i, j);
+//        return claimedEdges(box1);
+//    }
+//
+//    private int claimedEdges(Box b) {
+//        int filledSides = 0;
+//        if (b.getSide(Box.Side.NORTH)) {
+//            filledSides++;
+//        }
+//        if (b.getSide(Box.Side.SOUTH)) {
+//            filledSides++;
+//        }
+//        if (b.getSide(Box.Side.EAST)) {
+//            filledSides++;
+//        }
+//        if (b.getSide(Box.Side.WEST)) {
+//            filledSides++;
+//        }
+//        return filledSides;
+//    }
+//
+//    private boolean checkMove(int i, int j, Box.Side side) {
+//        if (currentBoard.hasPartner(currentBoard.getBox(i, j), side)) {
+//            Box newBox = currentBoard.getPartner(i, j, side);
+//            return claimedEdges(i, j, side)<=2 && claimedEdges(newBox)<=2;
+//        } else {
+//            return claimedEdges(i, j, side)<=2;
+//        }
+//
+//    }
+//
+//    private boolean scorableBox(Box b){
+//        return claimedEdges(b)==3;
+//    }
+//
+//    private boolean scorableBox(int i, int j){
+//        return scorableBox(currentBoard.getBox(i, j));
+//    }
+//
+//    private boolean determinePointMove() {
+//        for (int i = 0; i < currentBoard.getBoardSize(); i++) {
+//            for (int j = 0; j < currentBoard.getBoardSize(); j++) {
+//                //Box currentBox =
+//            }
+//        }
+//
+//        return false;
+//    }
+//
+//    private boolean determineSafeMove() {
+//        return false;
+//    }
+//
+//    private void randomMove() {
+//        int i, j, max, s;
+//        Box.Side side = null;
+//        max = 0;
+//
+//        i = rng.nextInt(max) + 1;
+//        j = rng.nextInt(max) + 1;
+//        s = rng.nextInt(4);
+//        switch (s) {
+//            case 0:
+//                side = Box.Side.EAST;
+//                break;
+//            case 1:
+//                side = Box.Side.SOUTH;
+//                break;
+//            case 2:
+//                side = Box.Side.WEST;
+//                break;
+//            case 3:
+//                side = Box.Side.NORTH;
+//        }
+//        if (checkMove(i, j, side)) {
+//            Board.Play(side, this.player, i, j);
+//        }
+//    }
+//
+//    public void makePlay(Board currentBoard) {
+//        this.currentBoard = currentBoard;
+//        boolean madeMove = false;
+//        while (!madeMove) {
+//
+//            if (!determinePointMove() && determineSafeMove()) {
+//                randomMove();
+//                madeMove = true;
+//            }
+//
+//            if (determinePointMove() && determineSafeMove() && !madeMove) {
+//                // take Point move
+//                madeMove = true;
+//            }
+//
+//            if (determinePointMove() && !determineSafeMove() && !madeMove) {
+//                // recurse
+//                madeMove = true;
+//            }
+//
+//            if (!determinePointMove() && !determineSafeMove() && !madeMove) {
+//                //
+//                madeMove = true;
+//            }
+//        }
+//    }
+//}
