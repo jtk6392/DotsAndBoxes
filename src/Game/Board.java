c0dd90aaa57c763daf73ff0a0c854d902bf17890
nsp5488 <nsp5488@g.rit.edu>
2019-02-16 19:10:48 -0500

    IT WORKS

--- src/Game/Board.java
+++ src/Game/Board.java
@@ -174,5 +174,12 @@ public class Board {
         Board b = new Board();
         System.out.println("test");
         System.out.println(b);
+        Users p1 = Users.PLAYER1;
+        Users p2 = Users.PLAYER2;
+        b.Play(Box.Side.EAST, p1, 0,0);
+        b.Play(Box.Side.NORTH,p2,0,0);
+        b.Play(Box.Side.SOUTH,p1,0,0);
+        b.Play(Box.Side.WEST, p2, 0,0);
+        System.out.println(b);
     }
 }
